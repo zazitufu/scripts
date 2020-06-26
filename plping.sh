365c96c79f4714a0be01c276ba6a0721975da66d
zazitufu <57139031+zazitufu@users.noreply.github.com>
2020-06-26 13:49:54 +0800

    Update plping.sh
    1、修改系统版本判断后，不同系统的执行方案使用变量，不再使用多行重复代码。
    2、修改预处理文件内回车符的方式
    3、修改获取Loss百分数的方式，不再受到列数变化的影响。

--- plping.sh
+++ plping.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# 版本 0.1.1
-# 此版本开始进行OS判断，因MAC OS 与 Linux 有些运行参数不同。
+# 版本 0.1.2     2020年6月26日
+# 主要update：系统版本判断后的动作，修改为变量更替，不再使用多行重复代码。
+# eg: ./plping ipfile
+# eg: ./plping ipfile 100
+##
 # 记录开始时间
 start_time=`date "+%Y-%m-%d %H:%M:%S"`
 sleep 1
@@ -30,12 +33,17 @@ report=$iplist.logb
 total_line=$(sed -n '$=' $iplist)
 current_line=0
 ##
-#### 进行Linux系统 和 MAC OS X的区分运行 ##############################################
-## GNU/Linux 系统下的运行
+# 预处理输入文件，将回车符号^M删掉。Windows和Dos下编辑的文件有可能产生这个问题。
+sed -i "s/\r//g" $iplist
+##
+#### 进行Linux系统 和 MAC OS X的区分 时间转换的参数设定 ####################
 if [ "$(uname)" = "Linux" ];then
-echo "Running on Linux"
-# 预处理输入文件，将符号^M删掉。Windows和Dos下编辑文件有可能产生这个问题。
-sed -i "s/\x0D//g" $iplist
+    echo "Running on Linux"
+    option_date=$(echo "-d")
+  elif [ "$(uname)" = "Darwin" ];then
+    echo echo "Running on MacOS"
+    option_date=$(echo "-j -f \"%Y-%m-%d %H:%M:%S\"")
+fi
 ##
 # 开始处理输入文件内的IP
 echo "## ↓↓↓ $(date) ↓↓↓ ######" >> $sum_report
@@ -49,7 +57,7 @@ do
    current_note=$(echo $LINE | awk '{print $2}')
    echo $current_note >> $report
    ping -q -c $runtimes $current_ip | sed '1,2d' >> $report
-   los_avg=$(echo -e Loss:$(echo $(tail -n 4 $report | grep "packet loss") | awk '{print $6}') Avg:$(echo $(tail -n 3 $report | grep "avg") | awk -F"/" '{print $5}'))
+   los_avg=$(echo -e Loss:$(echo $(tail -n 4 $report | grep -Eo "[0-9]+*%") Avg:$(echo $(tail -n 3 $report | grep "avg") | awk -F"/" '{print $5}')))
    echo $'\n' >> $report
    echo "$current_line of $total_line $los_avg Addr: $LINE " >> $sum_report
    echo -e "\033[36m$current_line \033[37mof \033[35m$total_line \033[33m$los_avg \033[32mFinished: \033[36m$current_ip \033[33m$current_note\033[0m"
@@ -58,38 +66,7 @@ echo
 ##
 # 脚本完成时间，运行总时长统计
 finish_time=`date "+%Y-%m-%d %H:%M:%S"`
-duration=$(echo $(($(date +%s -d "$finish_time")-$(date +%s -d "$start_time"))) | awk '{t=split("60 s 60 m 24 h 999 d",a);for(n=1;n<t;n+=2){if($1==0)break;s=$1%a[n]a[n+1]s;$1=int($1/a[n])}print s}')
-## Linux 结束
-##
-##################################################
-## Mac OS X 系统下的运行  	
-  elif [ "$(uname)" = "Darwin" ];then  
-echo "Running on MacOS"
-# 开始处理输入文件内的IP
-echo "## ↓↓↓ $(date) ↓↓↓ ######" >> $sum_report
-echo >> $sum_report
-echo "Start time: $start_time" >> $report
-while read LINE  || [[ -n ${LINE} ]]
-do
-	 ((current_line++))
-   echo "#### ↓↓↓  $current_line of $total_line ↓↓↓ ##########################################" >> $report
-   current_ip=$(echo $LINE | awk '{print $1}')
-   current_note=$(echo $LINE | awk '{print $2}')
-   echo $current_note >> $report
-   ping -q -c $runtimes $current_ip | sed '1,2d' >> $report
-   los_avg=$(echo -e Loss:$(echo $(tail -n 4 $report | grep "packet loss") | awk '{print $7}') Avg:$(echo $(tail -n 3 $report | grep "avg") | awk -F"/" '{print $5}'))
-   echo $'\n' >> $report
-   echo "$current_line of $total_line $los_avg Addr: $LINE " >> $sum_report
-   echo -e "\033[36m$current_line \033[37mof \033[35m$total_line \033[33m$los_avg \033[32mFinished: \033[36m$current_ip \033[33m$current_note\033[0m"
-done < $iplist
-echo
-##
-# 脚本完成时间，运行总时长统计
-finish_time=`date "+%Y-%m-%d %H:%M:%S"`
-duration=$(echo $(($(date -j -f "%Y-%m-%d %H:%M:%S" "$finish_time" +%s )-$(date -j -f "%Y-%m-%d %H:%M:%S" "$start_time" +%s))) | awk '{t=split("60 s 60 m 24 h 999 d",a);for(n=1;n<t;n+=2){if($1==0)break;s=$1%a[n]a[n+1]s;$1=int($1/a[n])}print s}')
-## MAC OS X 结束
-fi 
-#### 分系统运行结束 ##################################################
+duration=$(echo $(($(date $option_date "$finish_time" +%s )-$(date $option_date "$start_time" +%s))) | awk '{t=split("60 s 60 m 24 h 999 d",a);for(n=1;n<t;n+=2){if($1==0)break;s=$1%a[n]a[n+1]s;$1=int($1/a[n])}print s}')
 ##
 # 输出时间到文件
 echo "Finish Time: $(date)" >> $report
@@ -103,3 +80,4 @@ echo -e "\033[34mFinish Time:\033[0m $(date)"
 echo -e "\033[34mOutput File:\033[0m $sum_report   \033[34mDetail File:\033[0m $report"
 echo -e "\033[34mDuration of this script: \033[0m$duration    \033[34mCount: \033[0m$runtimes" 
 echo
+# 脚本完结撒花 O(∩_∩)O
