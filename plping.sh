eb5f0f1215c7eb09d3430b209ab9f2da2b2041b2
zazitufu <57139031+zazitufu@users.noreply.github.com>
2020-06-27 15:43:57 +0800

    Update plping.sh
    主要修改
    1、 48、49 行。删除 \r 的代码，从`sed s/\r//' ` 修改为 ` tr -d '\015' `
    2、 52行。获取掉包数值的代码，从固定域，修改为根据“loss”的位置前推2的相对域。

--- plping.sh
+++ plping.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# 版本 0.1.4     2020年6月27日
+# 版本 0.1.5     2020年6月27日
 # 主要update：各函数使用Linux和Mac OS通用的参数。不再进行系统版本判断。
 # 不再修改源文件处理回车符，改为在内存处理，源文件保持不变。
 # eg: ./plping ipfile
 # eg: ./plping ipfile 100
 ##
-version=0.1.4
-btime=2020-06-27
+version=0.1.5
+btime=2020-06-26
 # 记录开始时间
 start_time=`date +%s`
 start_time2=$(date)
@@ -45,11 +45,11 @@ while read LINE  || [[ -n ${LINE} ]]
 do
 	 ((current_line++))
    echo "#### ↓↓↓  $current_line of $total_line ↓↓↓ ##########################################" >> $report
-   current_ip=$(echo $LINE | sed 's/\r//' | awk '{print $1}')
-   current_note=$(echo $LINE | sed 's/\r//' | awk '{print $2}')
+   current_ip=$(echo $LINE | tr -d '\015' | awk '{print $1}')
+   current_note=$(echo $LINE | tr -d '\015' |awk '{print $2}')
    echo $current_note >> $report
    ping -q -c $runtimes $current_ip | sed '1,2d' >> $report
-   los_avg=$(echo -e Loss:$(echo $(tail -n 4 $report | grep "packet loss") | awk '{print $6}') Avg:$(echo $(tail -n 3 $report | grep "avg") | awk -F"/" '{print $5}')))
+   los_avg=$( echo -e Loss:$( tail -n 4 $report |  awk -F"[, ]" -v str="loss" '{v="";for (i=1;i<=NF;i++)  if ($i==str) v=v?"":i;if (v) print $(v-2)}' ) Avg:$( tail -n 3 $report | grep "avg" | awk -F"/" '{print $5}'))
    echo $'\n' >> $report
    echo "$current_line of $total_line $los_avg Addr: $current_ip $current_note " >> $sum_report
    echo -e "\033[36m$current_line \033[37mof \033[35m$total_line \033[33m$los_avg \033[32mFinished: \033[36m$current_ip \033[33m$current_note\033[0m"
