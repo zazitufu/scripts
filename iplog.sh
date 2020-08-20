f73100410dbdbc94fb45ed512502de8a90487c44
zazitufu <57139031+zazitufu@users.noreply.github.com>
2020-08-20 20:55:54 +0800

    Create iplog.sh
    1、记录家宽公网IP的变化。
    2、用在家里软路由上（不关机），用crontab计划任务，每5分钟执行一次脚本。
    3、获取公网ip的方式为curl my.ipip.net 。也可以用其他网站替换。但是返回值的awk域会变化

---
+++ iplog.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+### 用来记录公网IP的变化。使用crontab定时执行此脚本。sed不适配Darwin
+### eg:
+### ./iplog.sh ./outputfilename
+### 2020年8月20日 20点34分
+### 版本0.0.2
+reportfile=$1
+if $(tail -n 1 "$reportfile" | grep -q "Check") ;then
+ sed -i '/Check/d' $reportfile
+fi
+last_ip=$(tail -n 1 $reportfile|awk -F"[ ：]" '{print $7}')
+c_ip=$(curl myip.ipip.net |sed -e "s/^/$(date "+%Y-%m-%d %H:%M:%S") /" )
+n_ip=$(echo $c_ip|awk -F"[ ：]" '{print $7}')
+if [ ! -n "$c_ip" ] && [ "$last_ip" != "$n_ip" ];then
+	echo $c_ip >> $reportfile
+fi 
+echo Check Time: $(date "+%Y-%m-%d %H:%M:%S") >> $reportfile
