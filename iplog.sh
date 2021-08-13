3c672582d48d3bf6591bfc5e6e09996232983cf7
zazitufu <57139031+zazitufu@users.noreply.github.com>
2021-08-13 14:40:26 +0800

    Update iplog.sh
    有时会返回html内容，造成混乱，增加了过滤器过滤掉这类返回内容。
    <<!DOCTYPE html> 2021-08-06 14:40:00 <html> 2021-08-06 14:40:00 <head> 2021-08-06 14:40:00 <meta name="viewport" content="width=device-width,user-scalable=no" /> 2>

--- iplog.sh
+++ iplog.sh
@@ -2,14 +2,15 @@
 ### 用来记录公网IP的变化。使用crontab定时执行此脚本。sed不适配Darwin
 ### eg:
 ### ./iplog.sh ./outputfilename
-### 2020年8月20日 20点34分
-### 版本0.0.2
+### 2021年8月13日 14点37分
+### 有时会返回html内容，造成混乱，增加了过滤器过滤掉这类返回内容。
+### 版本0.0.4
 reportfile=$1
 if $(tail -n 1 "$reportfile" | grep -q "Check") ;then
  sed -i '/Check/d' $reportfile
 fi
 last_ip=$(tail -n 1 $reportfile|awk -F"[ ：]" '{print $7}')
-c_ip=$(curl myip.ipip.net |sed -e "s/^/$(date "+%Y-%m-%d %H:%M:%S") /" )
+c_ip=$(curl -s myip.ipip.net |sed -e "s/^/$(date "+%Y-%m-%d %H:%M:%S") /" |sed -e '/html/d')
 n_ip=$(echo $c_ip|awk -F"[ ：]" '{print $7}')
 if [ -n "$c_ip" ] && [ "$last_ip" != "$n_ip" ];then
 	echo $c_ip >> $reportfile
