f57b8ddc5854a736bc31e80065f5bed081d42b8c
zazitufu <57139031+zazitufu@users.noreply.github.com>
2022-03-30 02:32:13 +0800

    Create dlscripts.sh

---
+++ dlscripts.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+### 批量下载自制脚本文件
+scriptfile=(
+	plmtr.sh
+	plping.sh
+	iplog.sh
+	chomz.sh
+	acme320.sh
+	prettyping
+	publish.sh
+	traffic.sh
+	testip.sh
+  dlscripts.sh
+  lastbcount.sh
+	)
+for filename in ${scriptfile[@]}
+do
+	curl -O https://raw.githubusercontent.com/zazitufu/scripts/master/$filename
+	chmod +x $filename
+done
