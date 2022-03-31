f2f36348b6d42f49bc4c7a1e1d27869959718c4e
zazitufu <57139031+zazitufu@users.noreply.github.com>
2022-03-31 13:42:26 +0800

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 ### 获取订阅内容，写入本地文件，并将本地文件并传送至订阅连接。
-## Version 0.1
-## 2022年3月22日17点18分
-read -p "Input Subscribe:" subscribe
+## Version 0.2
+## 2022年3月31日13点40分
+read -e -p "Input Subscribe:" subscribe
 read -s -p "Remote Host Password:" pwd1
 echo $subscribe > filename
-scp -P port filename user@hostname:/www/wwwroot/pathname/
+#scp -P port filename user@hostname:/www/wwwroot/pathname/
+#ssh -p port user@hostname "echo $subscribe > www/wwwroot/pathname/kk"
+echo $subscribe > kk
 cat << EOF >/dev/null
 $pwd1
 EOF
