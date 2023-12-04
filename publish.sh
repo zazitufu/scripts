fa69a19e64d28f1142e969920f57516ef4a153ff
zazitufu <57139031+zazitufu@users.noreply.github.com>
2023-12-05 01:05:13 +0800

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-### 获取订阅内容，写入本地文件，并将本地文件并传送至订阅连接。
-## Version 0.2
-## 2022年3月31日13点40分
-read -e -p "Input Subscribe:" subscribe
-read -s -p "Remote Host Password:" pwd1
-echo $subscribe > filename
-#scp -P port filename user@hostname:/www/wwwroot/pathname/
-#ssh -p port user@hostname "echo $subscribe > www/wwwroot/pathname/kk"
-echo $subscribe > kk
-cat << EOF >/dev/null
-$pwd1
-EOF
+### 获取订阅内容并将其传送至远程服务器上的文件
+
+# 版本信息和日期
+version="0.3"
+date="2023年12月5日"
+
+# 读取用户输入的订阅链接
+read -e -p "Input Subscribe: " subscribe
+# 读取远程主机存放订阅链接的文件名
+read -e -p "Subscribe file name:" subscribefile
+
+# 将订阅链接传送到远程服务器的文件中
+# 确保已设置 SSH 密钥认证，否则可能需要手动输入密码
+# 自行修改domain / port / user / hostname
+remote_path="/www/wwwroot/domain/$subscribefile"
+ssh -p port user@hostname "echo '$subscribe' > $remote_path" || echo "SSH 命令执行失败。"
+
+echo "脚本执行完毕。版本：$version，日期：$date"
